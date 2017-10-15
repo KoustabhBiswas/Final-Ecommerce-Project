78b145b866f103a08824dedc404cdfcbaacb2d21
KoustabhBiswas <biswas.koustabh@gmail.com>
2017-10-15 15:08:01 +0530

    Final Ecommerce Project on Mobile

---
+++ Final Ecommerce Project/Ecommerce_Project-master/project/ecommercebackend/src/main/java/com/ecommerce2/ecommercebackend/model/Cart.java
@@ -0,0 +1,57 @@
+package com.ecommerce2.ecommercebackend.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Cart {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int cartId;
+	private int totalItems;
+	private int totalPrice;
+	@OneToOne
+	private User user;
+	@OneToMany(fetch=FetchType.EAGER, mappedBy="cart")
+	private Set<CartItem> cartItem=new HashSet<CartItem>();
+	public Set<CartItem> getCartItem() {
+		return cartItem;
+	}
+	public void setCartItem(Set<CartItem> cartItem) {
+		this.cartItem = cartItem;
+	}
+	public int getCartId() {
+		return cartId;
+	}
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+	public int getTotalItems() {
+		return totalItems;
+	}
+	public void setTotalItems(int totalItems) {
+		this.totalItems = totalItems;
+	}
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	
+}
